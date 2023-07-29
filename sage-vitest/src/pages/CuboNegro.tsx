c61db002a61b0f20b501bcdfba9bcdf6f96a30f8
Camila Morales <camilamo9719@gmail.com>
2023-07-28 23:23:39 -0500

    Codigo finalizado

--- sage-vitest/src/pages/CuboNegro.tsx
+++ sage-vitest/src/pages/CuboNegro.tsx
@@ -5,13 +5,13 @@ import '../components/styles/CuboNegro.css'
 const CuboNegro = () => {
 
   return (
-    <div>
-      <h1>Aqui está el cubo negro</h1>
-      <p>Dale click para aumentar o disminuir el número de lados</p>
+    <div className='divCuboNegro'>
+      <h1 className='h1CuboNegro'>Aqui está el cubo negro</h1>
+      <p className='pCuboNegro'>Dale click para aumentar o disminuir el número de lados</p>
 
       <section className='sectionCuboNegro'>
         {/**Se pone como parámetro el número de 3 lados */}
-        
+
         <JuanJose startPoints={3} />
       </section>
     </div>
